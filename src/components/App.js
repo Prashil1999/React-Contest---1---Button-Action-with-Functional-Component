6d66ebcd3b6de091098b8e2b198490117f58267e
Prashil S Gupta <33949630+Prashil1999@users.noreply.github.com>
2020-10-21 20:58:47 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,21 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  let [info, setInfo] = React.useState("");
+  let [info, setInfo] = React.useState(false);
   const handleClick = () => {
-    setInfo(
-      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-    );
+    setInfo(true);
   };
   return (
     <div id="main">
       <button id="click" key="click" onClick={handleClick}>
         click
       </button>
-      <p id="para" key="para">
-        {info}
-      </p>
+      {info ? (
+        <p id="para" key="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : null}
     </div>
   );
 }
